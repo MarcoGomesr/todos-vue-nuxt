9a019a0880f62b4fd15f199349c2fc2467c2ed8a
Marco <marco@whipgo.com>
2018-04-29 11:15:57 +0100

    counter

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="pa4">
+    {{ counter }}
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </div>
+</template>
+
+<script>
+import {mapState, mapMutations} from 'vuex'
+
+export default {
+  computed:{
+    ...mapState({
+      counter: state => state.counter
+    })
+  },
+
+  methods:{
+    ...mapMutations([
+      'increment',
+      'decrement'
+    ])
+  }
+}
+
+
+</script>
